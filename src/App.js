4a9af459569c3a4cc61641c1b1fedd676254ece2
monysary <sary.mony@gmail.com>
2023-01-23 22:02:38 -0800

    added additional files and folder to build out components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from "./components/Header";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="bg-[#2C3039]">
-      <Header />
+      <Header>
+        <Nav />
+      </Header>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,16 @@
-export { default } from './Header.js'
+function Header(props) {
+    return (
+        <header className="fixed w-full z-50">
+            <nav className="bg-[#3C3A40] px-2 sm:px-4 drop-shadow-lg">
+                <div className="container flex flex-wrap items-center justify-between mx-auto">
+                    <a href="/" className="flex items-center">
+                        <span className="self-center text-[30px] font-semibold whitespace-nowrap text-white">Mony Sary</span>
+                    </a>
+                    {props.children}
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
