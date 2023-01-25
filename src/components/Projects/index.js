718632a068e4606d91ad1ea01f3d33c01ee71d59
monysary <sary.mony@gmail.com>
2023-01-24 20:09:08 -0800

    styled mobile responsiveness for project page title

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,7 +1,7 @@
 function Projects() {
     return (
         <div className="lg:mx-[100px] sm:mx-[40px] mx-[20px] sm:pt-[100px] pt-[60px]">
-            <h2 className="text-[#E4CAA5] text-[60px] pb-[30px] mr-[70%] border-b-4 border-[#C83649]">Projects</h2>
+            <h2 className="text-[#E4CAA5] md:text-[60px] text-[40px] md:pb-[30px] pb-[10px] lg:mr-[70%] md:mr-[60%] border-b-4 border-[#C83649]">Projects</h2>
             <div className="">
 
             </div>
