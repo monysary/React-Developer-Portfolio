9be7129b9a4b830f9daadaf8bfc954ec6700a0a8
monysary <sary.mony@gmail.com>
2023-01-25 15:11:52 -0800

    added some jsx for resume and contact page. added some styling to home page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,10 @@
 function Contact() {
+    return (
+        <div className="lg:mx-[100px] sm:mx-[40px] mx-[20px] sm:pt-[100px] pt-[60px]">
+            <h2 className="text-[#E4CAA5] md:text-[60px] text-[40px] md:pb-[30px] pb-[10px] lg:mr-[70%] md:mr-[60%] mr-[40%] border-b-4 border-[#C83649]">Contact</h2>
 
+        </div>
+    )
 }
 
 export default Contact;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,10 +3,11 @@ import profileImage from '../../assets/images/placeholder-profile.png'
 function Home() {
     return (
         <div className="lg:mx-[100px] sm:mx-[40px] mx-[20px] sm:pt-[100px] pt-[60px]">
-            <h1 className="text-[#E4CAA5] lg:pb-[50px] sm:pb-[40px] pb-[20px] lg:text-[70px] sm:text-[55px] text-[30px] leading-tight border-b-4 border-[#C83649]">
+            <h1 className="text-[#E4CAA5] lg:mx-[60px] lg:pb-[50px] sm:pb-[40px] pb-[20px] lg:text-[70px] sm:text-[55px] text-[30px] leading-tight">
                 Hi, I'm <span className="text-[#C83649]">Mony</span>. <br />
                 Let's build something together.
             </h1>
+            <div className='h-[4px] bg-[#C83649]'></div>
             <section className="flex sm:my-[40px] lg:mx-[60px] flex-col sm:flex-row">
                 <img alt="placeholder-profile" src={profileImage} className="xl:h-[300px] xl:w-[300px] sm:h-[250px] sm:w-[250px] h-auto w-auto sm:m-0 m-[20px]" />
                 <p className="lg:ml-[60px] sm:ml-[40px] my-auto text-[#E4CAA5] sm:text-[20px] xl:text-[30px] text-center">
