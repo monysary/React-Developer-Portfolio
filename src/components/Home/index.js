79b2a0d66ea6b1150748974f41685a9d8124a0f5
monysary <sary.mony@gmail.com>
2023-01-24 20:47:05 -0800

    added project cards in project page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -11,7 +11,7 @@ function Home() {
                 <img alt="placeholder-profile" src={profileImage} className="xl:h-[300px] xl:w-[300px] sm:h-[250px] sm:w-[250px] h-auto w-auto sm:m-0 m-[20px]" />
                 <p className="lg:ml-[60px] sm:ml-[40px] my-auto text-[#E4CAA5] sm:text-[20px] xl:text-[30px] text-center">
                     I am a <span className="text-[#C83649]">full stack web developer</span> based in Southern California.
-                    I have strong passion in coding and design and have always been interested in technology.
+                    I have strong passion for coding and learning and have always been interested in technology.
                     I truly enjoy the process of designing and building websites, so let’s build something together!
                 </p>
             </section>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,9 +1,38 @@
+import { useState } from "react";
+
 function Projects() {
+    const [project] = useState([
+        {
+            name: 'Project 1'
+        },
+        {
+            name: 'Project 2'
+        },
+        {
+            name: 'Project 3'
+        },
+        {
+            name: 'Project 4'
+        },
+        {
+            name: 'Project 5'
+        },
+        {
+            name: 'Project 6'
+        },
+    ])
+
     return (
         <div className="lg:mx-[100px] sm:mx-[40px] mx-[20px] sm:pt-[100px] pt-[60px]">
             <h2 className="text-[#E4CAA5] md:text-[60px] text-[40px] md:pb-[30px] pb-[10px] lg:mr-[70%] md:mr-[60%] border-b-4 border-[#C83649]">Projects</h2>
-            <div className="">
-
+            <div className="flex flex-wrap md:pt-[40px] pt-[20px] justify-evenly">
+                {project.map((project) => (
+                    <div key={project.name}
+                        className="bg-[#3C3A40] drop-shadow-lg lg:w-[30%] sm:w-[40%] w-[80%] h-[100px] mb-[40px]"
+                    >
+                        {project.name}
+                    </div>
+                ))}
             </div>
         </div>
     )
