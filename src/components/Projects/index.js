5a4796282c927cc103c460273b2979f605aa86b0
monysary <sary.mony@gmail.com>
2023-01-25 22:28:26 -0800

    added more styling to project page

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -9,60 +9,68 @@ function Projects() {
             tech: 'Handlebars, Tailwind, Node, Express, Socket.io, MySQL',
             image: bitsPleaseChat,
             link: 'https://damp-river-11459.herokuapp.com/',
+            github: 'https://github.com/monysary/Bits-Please-Chat',
         },
         {
             name: 'Board & Booze',
             tech: 'HTML, CSS, Foundation, JS, API',
             image: boardBooze,
             link: 'https://sdanimc.github.io/shire-board-game/',
+            github: 'https://github.com/sdanimc/shire-board-game',
         },
         {
             name: 'Project 3',
             tech: 'HTML/CSS/JS',
             image: '',
             link: '',
+            github: '',
         },
         {
             name: 'Project 4',
             tech: 'HTML/CSS/JS',
             image: '',
             link: '',
+            github: '',
         },
         {
             name: 'Project 5',
             tech: 'HTML/CSS/JS',
             image: '',
             link: '',
+            github: '',
         },
         {
-            name: 'Bits Please Chat',
-            tech: 'Handlebars, Tailwind, Node, Express, Socket.io, MySQL',
-            image: bitsPleaseChat,
-            link: 'https://damp-river-11459.herokuapp.com/',
+            name: 'Project 6',
+            tech: 'HTML/CSS/JS',
+            image: '',
+            link: '',
+            github: '',
         },
     ])
 
     return (
         <div className="lg:mx-[100px] sm:mx-[40px] mx-[20px] sm:pt-[100px] pt-[60px]">
             <h2 className="text-[#E4CAA5] md:text-[60px] text-[40px] md:pb-[30px] pb-[10px] lg:mr-[70%] md:mr-[60%] mr-[40%] border-b-4 border-[#C83649]">Projects</h2>
-            <div className="flex flex-wrap md:pt-[40px] pt-[20px] lg:justify-between justify-evenly gap-y-[40px]">
+            <div className="flex flex-wrap md:py-[40px] py-[20px] lg:justify-between justify-evenly gap-y-[40px]">
                 {project.map((project) => (
                     <div key={project.name}
-                        className="bg-[#3C3A40] drop-shadow-xl rounded-lg lg:w-[30%] sm:w-[40%] w-[80%] h-[450px]"
+                        className="bg-[#3C3A40] drop-shadow-xl rounded-lg lg:w-[30%] sm:w-[40%] w-[80%] h-[450px] grid grid-rows-[90%_auto] overflow-hidden"
                     >
-                        <a href={project.link}>
-                            <img alt={project.name} src={project.image} className="h-[60%] w-full" />
-                        </a>
-                        <div className="text-center px-[20px]">
-                            <a href={project.link} className="text-[#E4CAA5] md:text-[25px] text-[20px] font-medium hover:underline">
-                                {project.name}
+                        <div className="h-full">
+                            <a href={project.link}>
+                                <img alt={project.name} src={project.image} className="h-[70%] w-full" />
                             </a>
-                            <div className="h-[2px] bg-[#9EACB7] my-[10px]"></div>
-                            <p className="text-[#F5F5F5] pb-[20px] md:text-[16px] text-[14px]">
-                                {project.tech}
-                            </p>
+                            <div className="text-center px-[20px]">
+                                <a href={project.link} className="text-[#E4CAA5] md:text-[25px] text-[20px] font-medium hover:text-[#C83649]">
+                                    {project.name}
+                                </a>
+                                <div className="h-[2px] bg-[#9EACB7] my-[10px]"></div>
+                                <p className="text-[#F5F5F5] pb-[20px] md:text-[16px] text-[14px]">
+                                    {project.tech}
+                                </p>
+                            </div>
                         </div>
-                        <i className="fab fa-github"></i>
+                        <a href={project.github} className="m-auto text-[#E4CAA5] hover:text-[#C83649]">Github</a>
                     </div>
                 ))}
             </div>
