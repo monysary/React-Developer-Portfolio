44e95ec2cd9c005d4af7b90325a0a77f73ccd67f
monysary <sary.mony@gmail.com>
2023-01-25 17:15:56 -0800

    added background color to body tag via index.css to remove tailwind cdn need

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -47,21 +47,23 @@ function Projects() {
             <h2 className="text-[#E4CAA5] md:text-[60px] text-[40px] md:pb-[30px] pb-[10px] lg:mr-[70%] md:mr-[60%] mr-[40%] border-b-4 border-[#C83649]">Projects</h2>
             <div className="flex flex-wrap md:pt-[40px] pt-[20px] lg:justify-between justify-evenly gap-y-[40px]">
                 {project.map((project) => (
-                    <a key={project.name}
-                        href={project.link}
-                        className="bg-[#3C3A40] drop-shadow-xl rounded-lg lg:w-[30%] sm:w-[40%] w-[80%] h-[450px] border-4 border-transparent hover:border-[#9EACB7]"
+                    <div key={project.name}
+                        className="bg-[#3C3A40] drop-shadow-xl rounded-lg lg:w-[30%] sm:w-[40%] w-[80%] h-[450px]"
                     >
-                        <img alt={project.name} src={project.image} className="h-[60%] w-full"/>
+                        <a href={project.link}>
+                            <img alt={project.name} src={project.image} className="h-[60%] w-full" />
+                        </a>
                         <div className="text-center px-[20px]">
-                            <h3 className="text-[#E4CAA5] md:text-[25px] text-[20px] font-medium py-[10px]">
+                            <a href={project.link} className="text-[#E4CAA5] md:text-[25px] text-[20px] font-medium hover:underline">
                                 {project.name}
-                            </h3>
-                            <div className="h-[2px] bg-[#9EACB7]"></div>
-                            <p className="text-[#F5F5F5] mt-[10px] pb-[20px] md:text-[16px] text-[14px]">
+                            </a>
+                            <div className="h-[2px] bg-[#9EACB7] my-[10px]"></div>
+                            <p className="text-[#F5F5F5] pb-[20px] md:text-[16px] text-[14px]">
                                 {project.tech}
                             </p>
                         </div>
-                    </a>
+                        <i className="fab fa-github"></i>
+                    </div>
                 ))}
             </div>
         </div>
